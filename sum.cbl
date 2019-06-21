1856d165efbfae36c68f01b76d49e799796f22c7
wahidaleena <34679809+wahidaleena@users.noreply.github.com>
2019-06-21 15:51:18 +0530

    Update sum.cbl

--- sum.cbl
+++ sum.cbl
@@ -1,17 +1,17 @@
-IDENTIFICATION DIVISION.
-       PROGRAM-ID. ADDITION.
-       DATA DIVISION.
-       WORKING-STORAGE SECTION.
-       77 A PIC 9(4).
-       77 B PIC 9(4).
-       77 C PIC 9(4).
-       PROCEDURE DIVISION.
-       PARA.
-           DISPLAY "ENTER THE VALUE OF A".
-           ACCEPT A.
-           DISPLAY "ENTER THE VALUE OF B".
-           ACCEPT B.
-           COMPUTE C = A + B.
-           DISPLAY "THE RESULTANT VALUE IS".
-           DISPLAY C.
-           STOP RUN.
+       IDENTIFICATION DIVISION.
+              PROGRAM-ID. ADDITION.
+              DATA DIVISION.
+              WORKING-STORAGE SECTION.
+              77 A PIC 9(4).
+              77 B PIC 9(4).
+              77 C PIC 9(4).
+              PROCEDURE DIVISION.
+              PARA.
+                  DISPLAY "ENTER THE VALUE OF A".
+                  ACCEPT A.
+                  DISPLAY "ENTER THE VALUE OF B".
+                  ACCEPT B.
+                  COMPUTE C = A + B.
+                  DISPLAY "THE RESULTANT VALUE IS".
+                  DISPLAY C.
+                  STOP RUN.
